5db383488cc267e00992bd4e0acf455271c8b75b
nahuel de bellis <debellisnahuel@gmail.com>
2017-12-20 18:37:18 -0300

    saque comentarios

--- server.js
+++ server.js
@@ -12,18 +12,6 @@ http.createServer(function(req, res){
 		var html_string = html.toString();
 		var variables = html_string.match(/[^\{\}]+(?=\})/g);
 		var parametross = p(req);
-
-		/*if(req.url.indexOf("?") > 0){
-			var urlData = req.url.split("?");
-			var parametros = urlData[1].split("&");
-			let mapping = parametros.map(function(val){
-				let datos = val.split("=");
-				parametross[datos[0]] = datos[1];
-				console.log(datos[1]);
-			});
-		}*/
-
-
 		let mapping = variables.map(function(val, index){
 			var value = parametross[val];	
 			var output = html_string.replace("{"+val+"}", value);
